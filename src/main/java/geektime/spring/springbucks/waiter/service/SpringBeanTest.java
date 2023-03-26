d5cb9a611ca5ac1e003e679960435ec66ebe9c21
zj_wanghq <937776575@qq.com>
2023-03-26 11:04:55 +0800

    测试Spring Bean

--- src/main/java/geektime/spring/springbucks/waiter/service/SpringBeanTest.java
+++ src/main/java/geektime/spring/springbucks/waiter/service/SpringBeanTest.java
@@ -1,67 +1,9 @@
 package geektime.spring.springbucks.waiter.service;
 
-import geektime.spring.springbucks.waiter.model.Coffee;
-import geektime.spring.springbucks.waiter.repository.CoffeeCrudRepository;
-import geektime.spring.springbucks.waiter.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.money.Money;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+public interface SpringBeanTest {
 
-@Slf4j
-@CacheConfig(cacheNames = "CoffeeCache")
-public class SpringBeanTest {
-    @Autowired
-    private CoffeeRepository coffeeRepository;
-    @Autowired
-    private CoffeeCrudRepository coffeeCrudRepository;
-
-    public Coffee saveCoffee(String name, Money price) {
-        Coffee coffee = coffeeRepository.save(Coffee.builder().name(name).price(price).build());
-        return coffee;
-    }
-
-    @Cacheable
-    public List<Coffee> getAllCoffee() {
-        return coffeeRepository.findAll(Sort.by("id"));
-    }
-
-    public Coffee getCoffee(Long id) {
-//        return coffeeRepository.findById(id).get();
-        return coffeeRepository.getOne(id);
-    }
-
-    public Coffee getCoffee(String name) {
-        return coffeeRepository.findByName(name);
-    }
-
-    public List<Coffee> getCoffeeByName(List<String> names) {
-        return coffeeRepository.findByNameInOrderById(names);
-    }
-
-    public void deleteAllCoffee() {
-        coffeeRepository.deleteAll();
-    }
-
-    public List<Coffee> getAllById(Iterable iterable) {
-        return coffeeRepository.findAllById(iterable);
-    }
-
-    public List<Coffee> getCoffeeByPage(int page,int size) {
-        //int page = 0; //page:当前页的索引。注意索引都是从 0 开始的。
-        //int size = 3;// size:每页显示 3 条数据
-        PageRequest pageable= new PageRequest(page, size);
-
-        Page<Coffee> pageCoffee = coffeeCrudRepository.findAll(pageable);
-        List<Coffee> coffeeList = pageCoffee.getContent();
-        return coffeeList;
-    }
+       void springBeanTest();
 
 }
---
+++ src/main/java/geektime/spring/springbucks/waiter/service/SpringBeanTestImpl.java
@@ -0,0 +1,10 @@
+package geektime.spring.springbucks.waiter.service;
+
+public class SpringBeanTestImpl implements SpringBeanTest{
+
+
+       @Override
+       public void springBeanTest() {
+              System.out.println("测试Spring Bean通过XML转载");
+       }
+}
